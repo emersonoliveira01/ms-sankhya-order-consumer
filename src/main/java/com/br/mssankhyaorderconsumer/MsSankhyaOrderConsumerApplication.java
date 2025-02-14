9de5f97d949ea680ab029df37e2c94f2e8ab9179
emersonoliveira01 <emersongpi@hotmail.com>
2025-02-14 10:35:51 -0300

    start project

---
+++ src/main/java/com/br/mssankhyaorderconsumer/MsSankhyaOrderConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.br.mssankhyaorderconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSankhyaOrderConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsSankhyaOrderConsumerApplication.class, args);
+    }
+
+}
